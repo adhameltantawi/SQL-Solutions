81b6d5a20da454cd348bc85742450ea99c8246a9
Adham Eltantawi <adhameltantawi@gmail.com>
2026-06-26 16:29:11 +0300

    Add SQL query for weather observation station 19
    Add SQL query to calculate the rounded distance between max and min latitude and longitude.

---
+++ HackerRank/Aggregation/weather-observation-station-19.sql
@@ -0,0 +1,11 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-19
+
+SELECT ROUND(
+    POWER(
+        POWER(MAX(LAT_N) - MIN(LAT_N), 2) +
+        POWER(MAX(LONG_W) - MIN(LONG_W), 2),
+        .5
+    ),
+    4
+)
+FROM STATION;
